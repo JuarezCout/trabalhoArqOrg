75b45b56faeeb5e0fce38c5a8612de8cdf708aec
JuarezCout <juarezcout15.jl@gmail.com>
2016-10-19 02:43:12 -0200

    Update Barramento.java

--- Barramento.java
+++ Barramento.java
@@ -1,38 +1,81 @@
 package trabalhoArqOrg;
 
+import java.util.Scanner;
+
 public class Barramento {
 	
+	double tamMemoria, clock, largBarr;
+	
+	/**
+	 * @return the tamMemoria
+	 */
+	public double getTamMemoria() {
+		return tamMemoria;
+	}
+
+	/**
+	 * @param tamMemoria the tamMemoria to set
+	 */
+	public void setTamMemoria(double tamMemoria) {
+		this.tamMemoria = tamMemoria;
+	}
+
+	/**
+	 * @return the clock
+	 */
+	public double getClock() {
+		return clock;
+	}
+
+	/**
+	 * @param clock the clock to set
+	 */
+	public void setClock(double clock) {
+		this.clock = clock;
+	}
+
+	/**
+	 * @return the largBarr
+	 */
+	public double getLargBarr() {
+		return largBarr;
+	}
+
+	/**
+	 * @param largBarr the largBarr to set
+	 */
+	public void setLargBarr(double largBarr) {
+		this.largBarr = largBarr;
+	}
+
 	//metodo que leve um Pacote de dados entre uma classe e outra
 	//barramento(REMETENTE, DESTINATARIO, PACOTE)
-	
-	//metodo que 
 	public void barramentoDados(String dest, String remet, int[] pacote){
-		if(dest == "RAM" && remet == "E/A"){
+		
+		if(dest == "RAM"){
 			
-		} else if (dest == "CPU" && remet == "RAM"){
+		} else if (dest == "CPU"){
 			
-		} else if (dest == "RAM" && remet == "CPU"){
-			
-		} 
+		}
 	}
 	
 	public void barramentoEndereco(String dest, String remet, int[] pacote){
-		if(dest == "RAM" && remet == "E/A"){
+		if(dest == "E/A"){
 			
-		} else if (dest == "CPU" && remet == "RAM"){
+		} else if (dest == "CPU"){
 			
-		} else if (dest == "RAM" && remet == "CPU"){
+		} else if (dest == "RAM"){
 			
 		} 
 	}
 	
 	public void barramentoControle(String dest, String remet, int[] pacote){
-		if(dest == "RAM" && remet == "E/A"){
+		if(dest == "RAM"){
 			
-		} else if (dest == "CPU" && remet == "RAM"){
+		} else if (dest == "CPU"){
 			
-		} else if (dest == "RAM" && remet == "CPU"){
-			
-		} 
+		}
 	}
+	
+	
 }
