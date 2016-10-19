0aeebf98524e1bee57bca0117d910f3a1a6ec346
JuarezCout <juarezcout15.jl@gmail.com>
2016-10-19 02:46:15 -0200

    Update Cpu.java

--- Cpu.java
+++ Cpu.java
@@ -3,7 +3,8 @@ package trabalhoArqOrg;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Cpu {
+public class Cpu extends MemoriaRam {
+	int[] memoria;
 	int regA = -1, regB = -1, regC = -1, regD = -1, regCI = -1;
 	boolean procComando = false;
 	
@@ -72,11 +73,65 @@ public class Cpu {
 		regCI = cont;
 	}
 	
+	public int getMemoria(int posicao)
+	{
+		return memoria[posicao];
+	}
+	/**
+	 * Define o valor de determinada posição da memória.
+	 * 
+	 * @param int $posicao Posição que será inserido o valor na memória.
+	 * @param int $valor Valor que será colocado na posição da memória.
+	 */
+	public void setMemoria(int posicao, int valor)
+	{
+		memoria[posicao] = valor;
+	}
+	
 	public void procMov(int [] comando){
 		int val1 = comando [1];
 		int val2 = comando [2];
 		
+		if(val2 < -1){
+			switch (val2){
+			case -2:
+				val2= getRegA();
+				break;
+			case -3:
+				val2 = getRegB();
+				break;
+			case -4:
+				val2 = getRegC();
+				break;
+			case -5:
+				val2 = getRegD();
+				break;
+			default:
+				val2 = memoria[(val2 * -1) - 5];
+				
+		}	
 		
+		}
+		
+		if(val1 < -1){
+			switch (val1){
+			case -2:
+				setRegA(val2);
+				break;
+			case -3:
+				setRegB(val2);
+				break;
+			case -4:
+				setRegC(val2);
+				break;
+			case -5:
+				setRegD(val2);
+				break;
+			default:
+				int pos = (val1 * -1) - 5;
+				setMemoria(pos, val2);				
+			}
+		}	
 		
 	}
 	
@@ -105,20 +160,6 @@ public class Cpu {
 	
 	// Rodando o Emulador
 	public static void main(String[] args) throws IOException {
-		
-		double tamMemoria, clock, largBarr;
-		Scanner valMem   = new Scanner(System.in);
-		Scanner valClock = new Scanner(System.in);
-		Scanner valLarg  = new Scanner(System.in);
-	
-		System.out.println("Tamanho da Memoria: ");
-		tamMemoria = valMem.nextDouble();
-		
-		System.out.println("Clock: ");
-		clock = valClock.nextDouble();
-		
-		System.out.println("Tamanho da Memoria: ");
-		largBarr = valLarg.nextDouble();
 		
 		EntradaSaida i = new EntradaSaida();
 		i.lerArquivo();
