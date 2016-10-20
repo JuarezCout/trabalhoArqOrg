0e52edd2acff55f2f368263dc2c9b3e20d1d4ab0
JuarezCout <juarezcout15.jl@gmail.com>
2016-10-20 01:19:10 -0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package trabalhoArqOrg;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,10 +11,24 @@ public class Main {
 			Cpu cpu         = new Cpu();
 			MemoriaRam mRam = new MemoriaRam();
 			Gerenciador ger = new Gerenciador();
+			double ram, clock, larg;
+			Scanner c = new Scanner (System.in);
 			
+			//receber os dados do Tamanho da Ram, Clock e Largura do Barramento
+			System.out.println("Tamanho da ram:");
+			ram = c.nextDouble();
+			System.out.println("Frequencia do Clock:");
+			clock = c.nextDouble();
+			System.out.println("Largura do barramento:");
+			larg = c.nextDouble();
+			c.close();
 			
+			//salvar no programa os parametros para execução
+			Gerenciador.barr.setTamMemoria(ram);
+			Gerenciador.barr.setClock(clock);
+			Gerenciador.barr.setLargBarr(larg);
 			
-			ea.rodaEntradaSaida();
+			//ea.rodaEntradaSaida();
 	}
 
 }
