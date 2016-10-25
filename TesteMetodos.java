2c3a728edda880b110b17795325414b6a4ffe18f
JuarezCout <juarezcout15.jl@gmail.com>
2016-10-25 01:32:49 -0200

    Update TesteMetodos.java

--- TesteMetodos.java
+++ TesteMetodos.java
@@ -1,13 +1,29 @@
 package trabalhoArqOrg;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class TesteMetodos {
 
 	public static void main(String[] args) throws IOException {
-		EntradaSaida i = new EntradaSaida();
-		i.lerArquivo();
-
+		
+		/*double tamMemoria, clock, largBarr;
+		Scanner valMem   = new Scanner(System.in);
+		Scanner valClock = new Scanner(System.in);
+		Scanner valLarg  = new Scanner(System.in);
+	
+		System.out.println("Tamanho da Memoria: ");
+		tamMemoria = valMem.nextDouble();
+		
+		System.out.println("Clock: ");
+		clock = valClock.nextDouble();
+		
+		System.out.println("Tamanho da Memoria: ");
+		largBarr = valLarg.nextDouble();*/
+		
+		Gerenciador g = new Gerenciador();
+		g.rodaGerenciador();
+		
 	}
 
 }
