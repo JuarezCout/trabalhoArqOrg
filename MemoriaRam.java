9d5c60b536aa7bf1467ca88b6b764534f4974917
JuarezCout <juarezcout15.jl@gmail.com>
2016-11-03 23:56:46 -0200

    Update MemoriaRam.java

--- MemoriaRam.java
+++ MemoriaRam.java
@@ -4,48 +4,46 @@ import java.util.Scanner;
 
 public class MemoriaRam {
 	
+	private static final Object Integer = 1;
 	public static int[] memoria = { -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
 									-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1 };
 	public int posMemoria = 0;
-	public boolean verificaTemPedido = true, verificaTemDados = true, verificaBarramentoEndereco = false;
+	//public boolean verificaTemPedido = true, verificaTemDados = true, verificaBarramentoEndereco = false;
 	
 	public void rodaRam (){
-		while(verificaTemPedido){ //Loop para verificar se tem alguem pedindo endereço
-			if(verificaTemPedido != Gerenciador.barr.isPerguntaMemoria()){ // Se não tiver pedido de memoria, vai rodar até apareceu um pedido
-				while (!verificaTemPedido){
-					if(verificaTemPedido == Gerenciador.barr.isPerguntaMemoria()){
+		while(Gerenciador.barr.filaCont.contains(Integer)){ //Loop para verificar se tem alguem pedindo endereço
+			if(Gerenciador.barr.isBarrContLiberado() == false){ // Se não tiver pedido de memoria, vai rodar até apareceu um pedido
+				while (!Gerenciador.barr.barrContLiberado == false){
+					if(Gerenciador.barr.barrContLiberado == true){
 						break;
 					}
 					System.out.println("Não tem");
 			}
 		} //Executa quando tem algum pedido de endereço
 			System.out.println("Recebi a pergunta");
-			Gerenciador.barr.setPerguntaMemoria(false); // Seta false na variavel apos verificar que tem pedido
+			//Gerenciador.barr.setPerguntaMemoria(false); // Seta false na variavel apos verificar que tem pedido
 			Gerenciador.barr.setBarrContLiberado(true); //Libera o barramento de controle
 			posEspMemoria();
 			if(posEspMemoria()){ // Se tiver memoria, pergunta se pode mandar para a EntradaSaida o endereço
-				while (!verificaBarramentoEndereco){
-					if(verificaBarramentoEndereco != Gerenciador.barr.isBarrEndLiberado()){
-						while (!verificaBarramentoEndereco){
-							if (verificaBarramentoEndereco == Gerenciador.barr.isBarrEndLiberado()){ break;	}
-					}
-				} 
-					Gerenciador.barr.barramentoEndereco("E/A", posMemoria);
-					
-				}
+					if(Gerenciador.barr.isBarrEndLiberado() == false){
+						while (Gerenciador.barr.isBarrEndLiberado() == false){
+							if (Gerenciador.barr.isBarrEndLiberado() == true){ break;}
+						}	
+						Gerenciador.barr.barramentoEndereco("E/A", posMemoria);	
+					} 
 			} else { //Senão tiver, manda um aviso que encheu e se deseja continuar a processar apagando os comandos amis antigos
 				Scanner c = new Scanner(System.in);
 				System.out.println("Acabou a Memoria para salvar as instruçoes!\nDeseja continuar, apagando os comandos mais antigos? [SIM||NAO]");
 			    String resp = c.nextLine();
 			    if (resp == "SIM"){
 			    	esvaziaMemoria();
-			    	while (!verificaBarramentoEndereco){
-						if(verificaBarramentoEndereco != Gerenciador.barr.isBarrEndLiberado()){
-							while (!verificaBarramentoEndereco){
-								if (verificaBarramentoEndereco == Gerenciador.barr.isBarrEndLiberado()){ break;	}
+			    	while (!Gerenciador.barr.isBarrEndLiberado()){
+						if(Gerenciador.barr.isBarrEndLiberado() != Gerenciador.barr.isBarrEndLiberado()){
+							while (!Gerenciador.barr.isBarrEndLiberado()){
+								if (Gerenciador.barr.isBarrEndLiberado()){ break;	}
 						}
 					} 
-						Gerenciador.barr.barramentoEndereco("E/A", posMemoria);
+						Gerenciador.barr.barramentoEndereco("E/A", posMemoria);						
 					}
 			    } else {
 			    	System.out.println("Memoria vai continuar cheia, logo encerraremos o Emulador!");
@@ -57,7 +55,7 @@ public class MemoriaRam {
 			
 		}
 		
-		while (verificaTemDados){ 
+		/*while (verificaTemDados){ 
 			if (verificaTemDados != Gerenciador.barr.isPerguntaDados()){ // Se não tiver com o barramento liberado, vai ficar rodando até liberar
 				while (!verificaTemDados){
 					if(verificaTemDados == Gerenciador.barr.isPerguntaDados()){ break; }
@@ -74,7 +72,7 @@ public class MemoriaRam {
 				System.out.print(MemoriaRam.memoria[cont] + " ");
 			}
 			System.out.println("}");
-		}
+		}*/
 	}
 	
 	/**
