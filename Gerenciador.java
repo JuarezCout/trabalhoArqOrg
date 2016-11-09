2db8bac8fd51f15b6952dafc9fac55a8532b263d
JuarezCout <juarezcout15.jl@gmail.com>
2016-11-09 03:31:05 -0200

    Update Gerenciador.java

--- Gerenciador.java
+++ Gerenciador.java
@@ -5,31 +5,25 @@ import java.util.Scanner;
 
 public class Gerenciador {
 	
-	public static Barramento barr = new Barramento();
+	public static Barramento barr;
+	public static EntradaSaida entradaSaida;
+	public static MemoriaRam memoriaRam;
+	public static Cpu cpu;
 	  
-	public void rodaGerenciador () throws IOException{
-		/*double ram, clock, larg;
-		Scanner c = new Scanner (System.in);
-		//receber os dados do Tamanho da Ram, Clock e Largura do Barramento
-		System.out.println("Tamanho da ram:");
-		ram = c.nextDouble();
-		System.out.println("Frequencia do Clock:");
-		clock = c.nextDouble();
-		System.out.println("Largura do barramento:");
-		larg = c.nextDouble();
-		c.close();
-		
-		//salvar na Arquitetura os parametros para execução
-		barr.setTamMemoria(ram);
-		barr.setClock(clock);
-		barr.setLargBarr(larg);*/
-		
-		EntradaSaida ea = new EntradaSaida();
-                MemoriaRam ram = new MemoriaRam();
-                Thread tES = new Thread(ea);
-                tES.start();
-                Thread tRam = new Thread(ram);
-                tRam.start();	
+	public static void rodaGerenciador (int tamanhoRam, int clock, int larguraBarramento) throws IOException{	
+		//instanciando as classes
+		barr = new Barramento(clock, larguraBarramento);
+		entradaSaida = new EntradaSaida();
+        memoriaRam = new MemoriaRam(tamanhoRam);
+       // cpu = new Cpu();
+        
+        //threads
+        Thread tES = new Thread(entradaSaida);
+        tES.start();
+        Thread tRam = new Thread(memoriaRam);
+        tRam.start();	
+        //Thread tCpu = new Thread(cpu);
+       // tCpu.start();
 	}
 	
 	public void usouBarramento(){
