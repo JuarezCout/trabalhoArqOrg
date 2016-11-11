1d76ec8bb60e086aab80fcf8f3f51ec71ae19417
JuarezCout <juarezcout15.jl@gmail.com>
2016-11-11 03:24:02 -0200

    Update MemoriaRam.java

--- MemoriaRam.java
+++ MemoriaRam.java
@@ -22,6 +22,80 @@ public class MemoriaRam implements Runnable {
 		}
 	}
 	
+	@Override
+	public void run() {		
+		while (true){
+			/*//Mostrando as alterações na memoria
+			for (int i = 0; i < memoria.length; i++) {
+				if (i%32 == 0){System.out.println();}
+				System.out.print(memoria[i] + " |");
+			}
+			System.out.println();*/
+			
+			try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(EntradaSaida.class.getName()).log(Level.SEVERE, null, ex);
+            }
+			if(barramentoEndLivre){
+				//verifica se tem sinal de controle no barramento 
+				if (Gerenciador.barr.getFilaCont(0) == 1){
+					System.out.println("RAM: recebi sinal da ES");
+					filaControle = Gerenciador.barr.getFilaCont(0);
+					Gerenciador.barr.setNullFilaCont();
+				} else {
+					this.filaControle = 0;
+				}
+				
+				//manda endereço para quem mandou o sinal
+				if (filaControle != 0 && filaControle == 1){ //se o sinal é da e/s
+					this.posMemoria = posMemoriaDisponivel();
+					Gerenciador.barr.barramentoEndereco("E/A", posMemoria);
+					System.out.println("RAM: Mandei endereço para EntradaSaida");
+					this.filaControle = 0;
+					this.barramentoEndLivre = false; // ocupa o barramento de endereço
+				}
+				if (filaControle != 0 && filaControle == 2){ //se o sinal é da cpu
+						this.posMemoria = posMemoriaDisponivel();
+						Gerenciador.barr.barramentoEndereco("CPU", posMemoria);
+						this.barramentoEndLivre = false;
+					}
+			}
+			
+			//verifica se tem dados no barramento e salva num variavel local
+			if (!Gerenciador.barr.getFilaDad().isEmpty()) {
+				this.filaDados = Gerenciador.barr.getFilaDad();
+				System.out.println(filaDados.get(0)[0]);
+				Gerenciador.barr.setNullFilaDad();
+				System.out.println("RAM: recebi dados da ES");
+			} else {
+				this.filaDados = null;
+			}
+
+			// grava na memoria os dados recebidos
+			if (filaDados != null) {
+				for (int i = 0; i < filaDados.size(); i++) {
+					addMemoria(filaDados.get(i), this.posMemoria);					
+				}
+				this.filaDados = null;
+				System.out.println("RAM: Salvei os dados na memoria");
+				System.out.println("\n");
+				
+				for (int i = 0; i < memoria.length; i++) {
+					if (i%32 == 0){System.out.println();}
+					System.out.print(memoria[i] + " |");
+				}
+				
+				this.barramentoEndLivre = true;
+			
+			Gerenciador.barr.setFilaDad(null);
+		
+		}
+			
+		}
+	}
+
+	
 	/**
 	* Metodos para tratar sobre o recebimento e verificação da memoria
 	*/
@@ -75,72 +149,7 @@ public class MemoriaRam implements Runnable {
 			avanPosMemoria();
 	}
 
-	@Override
-	public void run() {		
-		while (true){
-			//Mostrando as alterações na memoria
-			/**System.out.println("Entrei RAM");
-			for (int i = 0; i < memoria.length; i++) {
-				if (i%32 == 0){System.out.println();}
-				System.out.print(memoria[i] + " |");
-			}
-			System.out.println();*/
-			
-			try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(EntradaSaida.class.getName()).log(Level.SEVERE, null, ex);
-            }
-			if(barramentoEndLivre){
-				//verifica se tem sinal de controle no barramento 
-				if (Gerenciador.barr.getFilaCont(0) == 1){
-					System.out.println("RAM: recebi sinal da ES");
-					filaControle = Gerenciador.barr.getFilaCont(0);
-					Gerenciador.barr.setNullFilaCont();
-				} else {
-					this.filaControle = 0;
-				}
-				
-				//manda endereço para quem mandou o sinal
-				if (filaControle != 0 && filaControle == 1){ //se o sinal é da e/s
-					this.posMemoria = posMemoriaDisponivel();
-					Gerenciador.barr.barramentoEndereco("E/A", posMemoria);
-					System.out.println("RAM: Mandei endereço para EntradaSaida");
-					this.barramentoEndLivre = false;
-				}
-				if (filaControle != 0 && filaControle == 2){ //se o sinal é da cpu
-					if(barramentoEndLivre){
-						this.posMemoria = posMemoriaDisponivel();
-						Gerenciador.barr.barramentoEndereco("CPU", posMemoria);
-						this.barramentoEndLivre = false;
-					}
-				}
-			}
-			
-			//verifica se tem dados no barramento e salva num variavel local
-			if (!Gerenciador.barr.getFilaDad().isEmpty()) {
-				filaDados = Gerenciador.barr.getFilaDad();
-				Gerenciador.barr.setNullFilaDad();
-			} else {
-				filaDados = null;
-			}
-
-			// grava na memoria os dados recebidos
-			if (filaDados != null) {
-				for (int i = 0; i < filaDados.size(); i++) {
-					addMemoria(filaDados.get(i), this.posMemoria);
-					System.out.println("\n\n\n\n");
-					this.barramentoEndLivre = true;
-				}
-			
-			
-			Gerenciador.barr.setFilaDad(null);
-		
-		}
-			
-		}
-	}
-
+	
 	/**
 	 * @param barramentoEndLivre the barramentoEndLivre to set
 	 */
